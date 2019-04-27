e8da2c0ebcceaf3555d2f773cac6ea52c876735d
Jacobo Martinez <jacobojavier98@gmail.com>
2019-04-27 00:13:16 -0400

    add Wepack config for dev and production.

---
+++ webpack.common.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: {
+    main: './src/index.js',
+    vendor: './src/vendor.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      }
+    ]
+  }
+}
